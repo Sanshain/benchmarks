ca2f0a3a56646a58f36432ef47a349cda9c2fd63
Alexandr <digital-mag@ya.ru>
2022-01-06 18:25:22 +0300

    change some screens

--- .wrk/test.sh
+++ .wrk/test.sh
@@ -1,1 +1,4 @@
 wrk -t12 -c400 -d30s http://172.18.0.4:8000/
+wrk -t4 -c100 -d3s http://172.18.0.4:8000/
+
+# @FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd docker-machine') DO @%i
--- .wrk/wrk.bat
+++ .wrk/wrk.bat
@@ -5,3 +5,4 @@ docker run -it --rm --name wrk_test --entrypoint=/bin/sh skandyla/wrk
 docker network connect pentest_default wrk_test
 
 :: docker network inspect pentest_default
+:: docker attach wrk_test
