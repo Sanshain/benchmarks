3c95c1e798ea3762a94e093b7cf2393b902cb8fd
Alexandr <digital-mag@ya.ru>
2022-01-18 17:18:56 +0300

    redis cluster as complete jar

---
+++ redis/start.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+nohup redis-server ./node_1/redis.conf&
+nohup redis-server ./node_2/redis.conf&
+nohup redis-server ./node_3/redis.conf&
+
+# sleep 5
+# yes yes | redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002
+
+# redis-server ./node_1/redis.conf
+# redis-server ./node_2/redis.conf
+# redis-server ./node_3/redis.conf
+
+
+# for number in 1 2 3
+# do
+#     echo $number
+#     nohup redis-server ./node_$number/redis.conf&
+# done
+
+# redis-cli -c -p 7000 
